d91aa02a6cc57d4040ccc777097872b0c7ba2520
糸永　翔 <itonagasho@itonagashounoMacBook-Air.local>
2025-08-06 12:55:34 +0900

    practice2 Q1回答

--- php-practice_2.php
+++ php-practice_2.php
@@ -1,6 +1,16 @@
 <?php
 // Q1 tic-tac問題
-
+for ($x = 1; $x <= 100; $x++) {
+  if ($x % 4 === 0 && $x % 5 === 0) {
+    echo 'tic-tac' . "\n";
+  } elseif ($x % 4 === 0) {
+    echo 'tic' . "\n";
+  } elseif ($x % 5 === 0) {
+    echo 'tac' . "\n";
+  } else {
+    echo "$x \n";
+  }
+}
 
 // Q2 多次元連想配列
 
