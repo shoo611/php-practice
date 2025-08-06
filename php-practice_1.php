571deb186103af4b4b58e98f58fe6c66d5dea95d
糸永　翔 <itonagasho@itonagashounoMacBook-Air.local>
2025-08-06 11:31:05 +0900

    Q13回答

--- php-practice_1.php
+++ php-practice_1.php
@@ -119,6 +119,31 @@ distinguishNum($number = 4);
 distinguishNum($number = 3);
 
 // Q13 関数とswitch文
+function evaluateGrade($grade)
+{
+  switch ($grade) {
+    case 'A':
+    case 'B':
+      echo '合格です。' . "\n";
+      break;
 
+
+    case 'C':
+      echo '合格ですが追加課題があります。' . "\n";
+      break;
+
+
+    case 'D':
+      echo '不合格です。' . "\n";
+      break;
+
+
+    default:
+      echo '判定不明です。講師に問い合わせてください。' . "\n";
+      break;
+  }
+}
+evaluateGrade($grade = 'A');
+evaluateGrade($grade = 'F');
 
 ?>
