bc2a38cdee420430166472d54978d1a477b022de
糸永　翔 <itonagasho@itonagashounoMacBook-Air.local>
2025-08-06 14:47:05 +0900

    Q2-2回答

--- php-practice_2.php
+++ php-practice_2.php
@@ -13,6 +13,7 @@ for ($x = 1; $x <= 100; $x++) {
 }
 
 // Q2 多次元連想配列
+// Q2-1
 $personalInfos = [
   [
     'name' => 'Aさん',
@@ -31,6 +32,30 @@ $personalInfos = [
   ],
 ];
 echo $personalInfos[1]['name'] . 'の電話番号は' . $personalInfos[1]['tel'] . 'です。';
+
+// Q2-2
+$personalInfos = [
+  [
+    'name' => 'Aさん',
+    'mail' => 'aaa@mail.com',
+    'tel' => '09011112222'
+  ],
+  [
+    'name' => 'Bさん',
+    'mail' => 'bbb@mail.com',
+    'tel' => '08033334444'
+  ],
+  [
+    'name' => 'Cさん',
+    'mail' => 'ccc@mail.com',
+    'tel' => '09055556666'
+  ],
+];
+foreach ($personalInfos as $key => $value) {
+  echo ($key + 1) . '番目の' . $value['name'] . 'のメールアドレスは' . $value['mail'] . 'で、電話番号は' . $value['tel'] . 'です。' . "\n";
+}
+
+// Q2-3
 
 // Q3 オブジェクト-1
 
